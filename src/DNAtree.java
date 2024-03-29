7d9fa92814e081413483d63fabda20a28af4df5a
jaywynes <jayden.wynes@gmail.com>
2024-03-29 14:23:12 -0400

    Merged DNAtree.java and Node.java files, current file in src dir

--- src/DNAtree.java
+++ src/DNAtree.java
@@ -6,10 +6,19 @@ public class DNAtree {
     private FileInputStream file;
     private Scanner scan;
 
+    //instance variables and constructor by Matt
+    private Node head;
+
+    //new DNAtrees have 1 node with level 0
+    DNAtree(){
+        head=new Node(0);
+    }
+
     /**
      * Validates filename inputted from std in
      * @param filename - filename to be validated
      * @return true if file is valid and scanner object was properly created, false if file is invalid
+     * @author Jayden W.
      */
     public boolean validateFile(String filename) {
         try {
@@ -30,7 +39,7 @@ public class DNAtree {
             System.out.println("Token:" + token);
             if (token.equals("insert")) {
                 token = scan.next();
-                insert(token);
+                //insert(token);
             } else if (token.equals("print")) {
                 print();
             }
@@ -47,20 +56,56 @@ public class DNAtree {
 
     /**
      * Method takes the inputted DNA string and attempts to insert it into the tree
-     * @param token - DNA string to be inserted
+     * @param root - current node
+     * @param sequence - DNA string to be inserted
+     * @param level - current level in tree
      * @return level the string was inserted at, -1 if the string already exists
+     * @author Matt
      */
-    public int insert(String token) {
-        return -1;
+    //insert by Matt
+    //returns level of insertion, if duplicate returns -1
+    Integer insert(Node root,String sequence,Integer level){
+        if (sequence.equals(root.getSequence())){
+            return -1;
+        }
+        if (root.isLeaf() && root.getSequence()==null){
+            root.setSequence(sequence);
+            return root.getLevel();
+        }
+        //recursive case
+        Integer temp=insert(root.getChild(findChild(sequence,level)),
+                sequence,level);
+        //update current node
+        if (root.isLeaf()){
+            insert(head,root.setInternal(),0);
+        }
+        return temp;
     }
 
-
-    //instance variables and constructor by Matt
-    private Node head;
-
-    //new DNAtrees have 1 node with level 0
-    DNAtree(){
-        head=new Node(0);
+    /**
+     * Returns the position insert should recursively call
+     * @param sequence - DNA string to be analyzed
+     * @param level - current level
+     * @return correct child to call insert into
+     * @author Matt
+     */
+    //returns the position insert should recursively call
+    //A=0,C=1,G=2,T=3,$=4
+    Integer findChild(String sequence,Integer level){
+        //sequence ended
+        if (sequence.length()==level){
+            return 4;
+        }
+        char child=sequence.charAt(level);
+        if (child=='A'){
+            return 0;
+        } else if (child=='C'){
+            return 1;
+        } else if (child=='G'){
+            return 2;
+        }
+        //T is only remaining option
+        return 3;
     }
 
 
